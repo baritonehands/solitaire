c1c6078681995ee8bb8a040924d4c4023655f365
Brian Gregg <brian.gregg@centro.net>
2021-11-23 15:34:48 -0600

    More mobile/scrolling fixes

--- assets/build/worker.js
+++ assets/build/worker.js
@@ -1,2 +1,4 @@
 importScripts("shared.js");
-'use strict';sg(O(["Hello, Worker!"]));
+(function(){
+'use strict';try{$APP.Ig($APP.Q(["Hello, Worker!"]))}catch(a){throw console.error("An error occurred when calling (solitaire.worker.core/main)"),a;};
+}).call(this);
