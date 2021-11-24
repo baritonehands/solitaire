64de5bcaef95c12a353965e5e9b2b7dc0d95a4d0
Brian Gregg <biscuitalmighty@gmail.com>
2021-11-24 02:48:54 -0600

    Undo and Storage functionality

--- assets/build/worker.js
+++ assets/build/worker.js
@@ -1,4 +1,4 @@
 importScripts("shared.js");
 (function(){
-'use strict';try{$APP.Ig($APP.Q(["Hello, Worker!"]))}catch(a){throw console.error("An error occurred when calling (solitaire.worker.core/main)"),a;};
+'use strict';try{$APP.sg($APP.O(["Hello, Worker!"]))}catch(a){throw console.error("An error occurred when calling (solitaire.worker.core/main)"),a;};
 }).call(this);
